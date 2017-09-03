22615f98d14f1d3a3e51730a0522096e3083f91d
yujinwatabe <15c1020043wy@edu.tech.ac.jp>
2017-09-03 20:09:23 +0900

    TGS用のコミット

---
+++ 渡部/movefloor.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movefloor : MonoBehaviour {
+    /// <summary>
+    /// 移動先のオブジェクトをmovetransformに入れると初期地点と移動先を往復します。
+    /// 移動先のオブジェクトはカラのゲームオブジェクトにしてください。
+    /// 移動先にオブジェクトがあった場合のことはまだ仕様が固まってないので未定です。
+    /// </summary>
+    [SerializeField]
+    private Transform movetransform;
+    [SerializeField]
+    private float movespeed=3;
+    [SerializeField]
+    private float stopwait=0;
+    [SerializeField]
+    private bool isrunning = true;
+    private int moveint=1;
+    private bool move_=false;
+    private Vector2[] movepos = new Vector2[2];
+    [SerializeField]
+    private Animator animator;
+    Rigidbody ri;
+    void Start () {
+        ri = GetComponent<Rigidbody>();
+        movetransform.LookAt(transform);
+        movepos[0] = transform.position;
+        movepos[1] = movetransform.position;
+	}
+    void Update() {
+        if (isrunning)
+        {
+            animator.speed =    1;
+            ri.constraints = RigidbodyConstraints.FreezeRotation;
+            animator.SetInteger("move", moveint);
+            animator.SetBool("movebool", move_);
+            if (!move_)
+            {
+                switch (moveint)
+                {
+                    case 0:
+                        ri.velocity= movetransform.forward * movespeed;
+                        //transform.position += movetransform.forward * movespeed * Time.deltaTime;
+                        break;
+                    case 1:
+                        ri.velocity = movetransform.forward * movespeed*-1;
+                        //transform.position += movetransform.forward * movespeed * -1 * Time.deltaTime;
+                        break;
+                }
+            }
+            if (!move_ && Vector2.Distance(transform.position, movepos[moveint]) < 0.5f )
+            {
+                move_ = true;
+                Invoke("movewait", stopwait);
+            }
+        }else
+        {
+            animator.speed = 0;
+        }
+    }
+    public void movewait()
+    {
+        
+        switch (moveint)
+        {
+            case 0:
+                moveint = 1;
+                break;
+            case 1:
+                moveint = 0;
+                break;
+        }
+        move_ = false;
+    }
+    public bool isRunning
+    {
+        get
+        {
+            return isrunning;
+        }
+        set
+        {
+            isrunning = value;
+        }
+    }
+    public void SetAnimationFrame(float i_frame)
+    {
+        var clipInfoList = animator.GetCurrentAnimatorClipInfo(0);
+        var clip = clipInfoList[0].clip;
+        var time = 15 * i_frame / clip.frameRate;
+        var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        var animationHash = stateInfo.shortNameHash;
+        animator.Play(animationHash, 0, time);
+    }
+}
---
+++ 鷲頭/poketWatch.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poketWatch : MonoBehaviour {
+    private Vector3 localGravity = new Vector3(0,-30f,0);
+    private Rigidbody rb;
+    private bool isTime = false;
+    private float limit;
+    [SerializeField]
+    private float limitTime;
+    private gyro2 Gyro;
+    private float[] setGyro = new float[2];
+    private float gravity = 15f;
+
+    void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        //rb.useGravity = false;
+        isTime = true;
+        limit = limitTime;
+        Gyro = GameObject.Find("Main Camera").GetComponent<gyro2>();
+
+    }
+
+    void Update () {
+        Debug.Log("GetNowGyro=" + Gyro.GetNowGyro);
+        setLocalGravity();
+        if (isTime)
+        {
+            limitTime -= Time.deltaTime;
+            //Debug.Log("TIMELIMIT=" + limitTime);
+        }
+        if (limitTime <= 0)
+        {
+            limitTime = limit;
+            isTime = false;
+        }
+
+    }
+
+    void setLocalGravity()
+    {
+        if (Gyro.GetNowGyro <= 90 || Gyro.GetNowGyro >= 270)
+        {
+            if (Gyro.GetNowGyro >= 270) setGyro[0] = 270 - Gyro.GetNowGyro;
+            else setGyro[0] = Gyro.GetNowGyro - 90;
+        }
+        else
+        {
+            if (Gyro.GetNowGyro <= 180) setGyro[0] = Gyro.GetNowGyro - 90;
+            else setGyro[0] = Gyro.GetNowGyro - 180;
+        }
+
+        if (Gyro.GetNowGyro <= 180)
+        {
+            if (Gyro.GetNowGyro <= 90) setGyro[1] = Gyro.GetNowGyro * -1;
+            else setGyro[1] = Gyro.GetNowGyro - 180;
+        }
+        else
+        {
+            if (Gyro.GetNowGyro <= 270) setGyro[1] = Gyro.GetNowGyro - 180;
+            else setGyro[1] = 360 - Gyro.GetNowGyro;
+            
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            setGyro[i] /= 90;
+        }
+
+        rb.velocity = new Vector3(setGyro[1] * gravity, setGyro[0] * gravity, 0);
+        rb.velocity *= -1;
+        //Physics.gravity = new Vector3();
+    }
+}
